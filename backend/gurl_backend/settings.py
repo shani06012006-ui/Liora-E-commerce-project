997a286deaa7885ec5755b691da58e70cfd1b6c0
shani06012006 <shani06012006@gmail.com>
2026-08-04 20:32:59 +0530

    Update CORS for current Vercel domain

--- backend/gurl_backend/settings.py
+++ backend/gurl_backend/settings.py
@@ -148,13 +148,13 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-    "https://liora-e-commerce-project.vercel.app",
+    "https://liora-ecommerce-project-sha.vercel.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://liora-e-commerce-project.vercel.app",
+    "https://liora-ecommerce-project-sha.vercel.app/",
 ]
 
 CORS_ALLOW_METHODS = [
