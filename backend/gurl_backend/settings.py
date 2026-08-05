dffbe3268d90f04cb854a1640ea14fdf48c6db19
shani06012006 <shani06012006@gmail.com>
2026-08-05 11:25:41 +0530

    Prepare backend settings for Render deployment

--- backend/gurl_backend/settings.py
+++ backend/gurl_backend/settings.py
@@ -148,7 +148,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-    "https://liora-ecommerce-project-sha.vercel.app/",
+    "https://liora-ecommerce-project-sha.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
